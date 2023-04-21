9eafa029e59fff5dab52d7c4bae58839488e0fb3
mortalpjero <mortalpjero@gmail.com>
2023-04-21 07:41:36 -0700

    fix: final maintability optimization for brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,32 +1,29 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
 
-// Defining correct answers
-
-const positiveAnswer = 'yes';
-const negativeAnswer = 'no';
-
-// Start of an app
-
 const userName = readlineSync.question('May I have your name? ');
 console.log(`Hello, ${userName}!`);
 
-// Guess if the number is even or not
-
 function playEvenNumberGame() {
-  for (let i = 0; i < 3;) {
+  const positiveAnswer = 'yes';
+  const negativeAnswer = 'no';
+  const maxAttempts = 3;
+  let attempts = 0;
+
+  while (attempts < maxAttempts) {
     console.log('Answer "yes" if the number is even, otherwise answer "no".');
-    const randomNum = Math.floor(Math.random() * 100);
-    console.log(`Question: ${randomNum}`);
+    const randomNumber = Math.floor(Math.random() * 100);
+    console.log(`Question: ${randomNumber}`);
     const userAnswer = readlineSync.question('Your answer: ');
 
-    if (
-      (randomNum % 2 === 0 && userAnswer === positiveAnswer)
-      || (randomNum % 2 !== 0 && userAnswer === negativeAnswer)) {
+    const isCorrectAnswer = (randomNumber % 2 === 0 && userAnswer === positiveAnswer)
+    || (randomNumber % 2 !== 0 && userAnswer === negativeAnswer);
+
+    if (isCorrectAnswer) {
       console.log('Correct!');
-      i += 1;
+      attempts += 1;
     } else {
-      console.log(`'${userAnswer}' is the wrong answer. The correct answer was '${randomNum % 2 === 0 ? positiveAnswer : negativeAnswer}'.`);
+      console.log(`'${userAnswer}' is the wrong answer. The correct answer was '${randomNumber % 2 === 0 ? positiveAnswer : negativeAnswer}'.`);
       console.log(`Let's try again, ${userName}!`);
       return;
     }
